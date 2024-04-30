9a2fbfc94d0f7baff2c2a3f0afd156802c079ebb
Dylan Fish <82403668+Wincey@users.noreply.github.com>
2024-04-29 20:53:01 -0500

    Update index.php
    moved head content into includes

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>"test"'s DataTable</title>
-    
-  </head>
+<head>
+  <?php include 'includes/head.php'; ?>
+</head>
 ￼
 ￼
 ￼
